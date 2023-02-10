eae1e78b09e38793dea1d14d09890f2fd031e082
Hendrix Tavarez <htavarez@gmail.com>
2023-02-09 21:41:12 -0500

    logisim

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class demo {
+  public static void main(String[] args) {
+    int x = Integer.MAX_VALUE;
+
+    System.out.println("Highest int value in Java is " + x);
+
+    System.out.println(x + 5);
+  }
+}
